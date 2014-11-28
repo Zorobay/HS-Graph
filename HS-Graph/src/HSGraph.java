18d6c414e18555768544dc9b50166a8bf033f87a
Sebastian Hegardt <se.hegardt@gmail.com>
2014-11-28 12:05:41 +0100

    Cloned to laptop, works fine

--- HS-Graph/src/HSGraph.java
+++ HS-Graph/src/HSGraph.java
@@ -1,8 +1,10 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 
@@ -16,22 +18,25 @@ public class HSGraph extends Application {
 
 	public void start(Stage primaryStage) throws Exception {
 		Button btn = new Button("Hi!");
-		Button btn2 = new Button("Apa!");
+		Label lbl = new Label();
+		
+		btn.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent event){
+				lbl.setText("You Smell Funny!");
+			}
+		});
+		
 		Group root = new Group();
 		Scene scene = new Scene(root, 400, 400);
 		
-		
 		root.getChildren().add(btn);
-		root.getChildren().add(btn2);
+		root.getChildren().add(lbl);
 		
 		btn.setLayoutY(scene.getHeight() / 2);
 		btn.setLayoutX(scene.getWidth() / 2);
 		
-		
-		
-		
 		primaryStage.setScene(scene);
-		primaryStage.setTitle("How the fuck does this work?");
+		primaryStage.setTitle("GUI leklåda!");
 		primaryStage.show();
 	}
 
