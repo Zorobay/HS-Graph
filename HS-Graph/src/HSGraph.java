7ba7109917034fe5d8e9ec8b1d150ec61bdfcdee
Sebastian Hegardt <se.hegardt@gmail.com>
2015-01-12 17:00:16 +0000

    commit before pull

--- HS-Graph/src/HSGraph.java
+++ HS-Graph/src/HSGraph.java
@@ -1,9 +1,14 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class HSGraph extends Application {
@@ -13,25 +18,70 @@ public class HSGraph extends Application {
 
 	}
 
+	int r = 0;
+	int g = 0;
+	int b = 0;
+
 	public void start(Stage primaryStage) throws Exception {
+
 		primaryStage.setTitle("Labels");
-		
-		Button b = new Button("hello!");
-		b.getStyleClass().add("bballs");
-		
+		Image img = new Image("file:images/ok-icon.png", 30, 30, true, true);
+		ToggleButton tb = new ToggleButton("Red");
+		tb.getStyleClass().add("redb");
+		ToggleButton tb2 = new ToggleButton("Green");
+		tb2.getStyleClass().add("greenb");
+		ToggleButton tb3 = new ToggleButton("Blue");
+		tb3.getStyleClass().add("blueb");
+		tb3.setTooltip(new Tooltip("The blue button"));
+
+		Rectangle rect = new Rectangle();
+		rect.setHeight(50);
+		rect.setWidth(135);
+		rect.setFill(Color.rgb(0, 0, 0));
+		rect.setArcHeight(10);
+		rect.setArcWidth(10);
+
 		GridPane grid = new GridPane();
 		grid.setAlignment(Pos.CENTER);
-		grid.setHgap(10);
+		grid.setHgap(5);
 		grid.setVgap(10);
 		grid.setPadding(new Insets(25, 25, 25, 25));
 		grid.setGridLinesVisible(false);
 
+		// action events
+		tb.setOnAction((ActionEvent e) -> {
+			if (r == 255)
+				r = 0;
+			else
+				r = 255;
+			rect.setFill(Color.rgb(r, g, b));
+		});
+
+		tb2.setOnAction((ActionEvent e) -> {
+			if (g == 255)
+				g = 0;
+			else
+				g = 255;
+			rect.setFill(Color.rgb(r, g, b));
+		});
+
+		tb3.setOnAction((ActionEvent e) -> {
+			if (b == 255)
+				b = 0;
+			else
+				b = 255;
+			rect.setFill(Color.rgb(r, g, b));
+		});
 		// add all components
-		grid.add(b, 0, 0);
-		
+		grid.add(tb, 0, 0);
+		grid.add(tb2, 1, 0);
+		grid.add(tb3, 2, 0);
+		grid.add(rect, 0, 1, 3, 1);
+
 		Scene scene = new Scene(grid);
 		primaryStage.setScene(scene);
-		scene.getStylesheets().add(HSGraph.class.getResource("style.css").toExternalForm());
+		scene.getStylesheets().add(
+				HSGraph.class.getResource("style.css").toExternalForm());
 		primaryStage.show();
 	}
 }
--- HS-Graph/src/canvas.java
+++ HS-Graph/src/canvas.java
@@ -13,24 +13,33 @@ public class canvas extends Application {
 	}
 
 	public void start(Stage primaryStage) throws Exception {
+		final int WIDTH = 500;
+		final int HEIGHT = 500;
 		primaryStage.setTitle("Canvas graphics test");
-		Canvas canvas = new Canvas(300, 250);
+		Canvas canvas = new Canvas(WIDTH, HEIGHT);
 		Group root = new Group();
-
-		root.getChildren().add(canvas);
-
-		for (int i = 0; i <= 200; i++) {
-			drawShapes(canvas.getGraphicsContext2D(), i, i * 2);
+		
+		//Draw Axes
+		canvas.getGraphicsContext2D().setLineWidth(0.5);
+		canvas.getGraphicsContext2D().strokeLine(WIDTH/2, 0, WIDTH/2, HEIGHT);
+		canvas.getGraphicsContext2D().strokeLine(0, HEIGHT/2, WIDTH, HEIGHT/2);
+		
+		//Draw graph y = x^(2)
+		for (int x = 0; x <= WIDTH/2; x++) { //for x >0
+			int m = -100;
+			drawLine(canvas, x, Math.pow(x*2, 2) -30*x, x + 1, Math.pow((x + 1)*2, 2) -30*(x+1));
 		}
-
+		
+		root.getChildren().add(canvas);
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 
-	public static void drawShapes(GraphicsContext gc, int x, double d) {
-		gc.setFill(Color.BLACK);
-		gc.fillRect(x, d, 1, 1);
+	public static void drawLine(Canvas c, int x, double y, int x2, double y2) {
+		c.getGraphicsContext2D().setLineWidth(2);
+		c.getGraphicsContext2D().setStroke(Color.YELLOWGREEN);
+		c.getGraphicsContext2D().strokeLine(x + c.getWidth()/2, c.getHeight()/2 - y, x2 + c.getWidth()/2, c.getHeight()/2 - y2);
 	}
 
 }
