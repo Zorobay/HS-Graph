f9a4c7f9df15f57e5ab6a2194d171335298a50a0
Sebastian Hegardt <se.hegardt@gmail.com>
2015-01-15 19:33:30 +0000

    Sebbe teaches balle about merge conflicts

--- HS-Graph/src/HSGraph.java
+++ HS-Graph/src/HSGraph.java
@@ -19,7 +19,8 @@ public class HSGraph extends Application {
 	}
 
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("HS-Graph");
+		primaryStage.setTitle("Snapsnopp flippflopp! :)");
+		//Clara är en morot
 
 		//Grid Pane Setup
 		GridPane grid = new GridPane();
